da124d396c6ae5ceacc2dcb11abee6739624877a
Prattay Dey <prattay2005@gmail.com>
2025-04-08 22:49:28 -0400

    Update motion_detection.py

--- motion_detection.py
+++ motion_detection.py
@@ -4,10 +4,9 @@ import numpy as np
 import cv2
 from picamera2 import Picamera2
 
-# Create the 'screenshots' folder if it doesn't exist
 os.makedirs("screenshots", exist_ok=True)
 
-# Initialize the Raspberry Pi camera (v1.3) using Picamera2
+# Raspberry Pi camera (v1.3)
 picam2 = Picamera2()
 picam2.configure(picam2.create_still_configuration())
 picam2.start()
@@ -16,12 +15,17 @@ last_frame = None  # used for comparisons
 detected_motion = False
 prev_detected_motion = False  # to track changes in motion state
 screenshot_count = 0
+start_time = time.time()
 
 while True:
-    # Capture a frame from the Pi camera
+    # capture a frame from the Pi camera
     frame = picam2.capture_array()
+    
+    # check how much time has passed (10s for now)
+    if time.time() - start_time >= 10:  
+        break
 
-    # Convert to grayscale and blur to reduce noise
+    # convert to grayscale and blur to reduce noise
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -29,14 +33,14 @@ while True:
         last_frame = gray
         continue
 
-    # Compute the absolute difference between the current frame and the previous frame
+    # compute the absolute difference between the current frame and the previous frame
     frame_diff = cv2.absdiff(last_frame, gray)
     _, threshold = cv2.threshold(frame_diff, 25, 255, cv2.THRESH_BINARY)
 
-    # Count the number of white pixels (i.e., regions of change/motion)
+    # count the number of white pixels (i.e., regions of change/motion)
     motion_pixels = cv2.countNonZero(threshold)
 
-    # Update the motion flag based on a threshold (here, 1000 pixels)
+    # update the motion flag based on a threshold (here, 1000 pixels)
     if motion_pixels > 1000:
         print("Motion detected!")
         detected_motion = True
@@ -44,7 +48,7 @@ while True:
         print("No motion")
         detected_motion = False
 
-    # Take a screenshot when the motion detection state changes
+    # take a screenshot when the motion detection state changes
     if detected_motion != prev_detected_motion:
         screenshot_name = f"screenshots/screenshot_{screenshot_count}.png"
         cv2.imwrite(screenshot_name, frame)
@@ -54,14 +58,17 @@ while True:
     # Update the previous state and the last frame for the next iteration
     prev_detected_motion = detected_motion
     last_frame = gray
+    
+    # Resize the frame to make it smaller (ex 640x480)
+    resized_frame = cv2.resize(frame, (320, 240))
+    
 
     # Display the current frame for debugging
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', resized_frame)
 
     # Exit if the user presses 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Cleanup resources
 picam2.stop()
 cv2.destroyAllWindows()
